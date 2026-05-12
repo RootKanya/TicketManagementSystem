5db980c5a7138774a33352c97ca42d41f7f0ce27
RootKanya <berlianyafikaniamuawanah@gmail.com>
2026-05-13 02:18:25 +0700

    Feat: Add Value Objects Money.cs

---
+++ Ticketing.Domain/ValueObjects/money.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Domain.ValueObjects;
+public record Money(decimal Amount, string Currency)
+{
+    public static Money Zero(string currency = "IDR") => new(0, currency);
+    public static Money Create(decimal amount, string currency = "IDR")
+    {
+        if (amount < 0)
+            throw new ArgumentException("Price cannot be negative.");
+
+        return new Money(amount, currency);
+    }
+}
