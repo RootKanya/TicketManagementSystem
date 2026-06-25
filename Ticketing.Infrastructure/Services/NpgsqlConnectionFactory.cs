e2cb41503458934e339b850ba36d4565f5cda61d
azkafh <azkafauziy@gmail.com>
2026-06-25 21:32:42 +0700

    feat: add npgsql connection

---
+++ Ticketing.Infrastructure/Services/NpgsqlConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Data;
+using Ticketing.Application.ExternalInterfaces;
+
+namespace Ticketing.Infrastructure.Services
+{
+    public class NpgsqlConnectionFactory : IQueryConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public NpgsqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new ArgumentNullException("Database connection string is missing in appsettings.json.");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
