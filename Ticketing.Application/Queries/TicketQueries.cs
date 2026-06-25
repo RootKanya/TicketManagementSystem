74d750f3b6474973cd9d0188e67ae3f819bacd39
RootKanya <berlianyafikaniamuawanah@gmail.com>
2026-06-25 18:07:48 +0700

    Add purchased tickets query and handler

---
+++ Ticketing.Application/Queries/TicketQueries.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using Ticketing.Application.DTOs;
+
+namespace Ticketing.Application.Queries
+{
+    public class ViewPurchasedTicketsQuery : IRequest<IEnumerable<PurchasedTicketDto>>
+    {
+        public Guid CustomerId { get; set; }
+    }
+}
