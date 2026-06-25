796595034aa62524d7b49fab0e663baa47fa546e
RootKanya <berlianyafikaniamuawanah@gmail.com>
2026-06-25 17:30:14 +0700

    Feat : Add commands, services, and tests for event management

---
+++ Ticketing.Domain/Enums/RefundStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Domain.Enums
+{
+    public enum RefundStatus
+    {
+        Requested,
+        Approved,
+        Rejected,
+        PaidOut
+    }
+}
